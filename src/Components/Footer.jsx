9b8ca1ce31d69aed5ad1166e8c3f71b6065489ea
RuinerAl <amanladla2000@gmail.com>
2021-09-03 12:26:52 +0530

    Initial Commit

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,14 @@
+
+function Footer(){
+
+    return(
+        <footer>
+            <p>
+            Copyright © 2021
+            </p>
+        </footer>
+    );
+
+}
+
+export default Footer;
---
+++ src/Components/Header.jsx
@@ -0,0 +1,14 @@
+
+function Header(){
+
+    return (
+        <div>
+            <header>
+                <h1>Keeper Application</h1>
+            </header>
+        </div>
+    );
+
+}
+
+export default Header;
